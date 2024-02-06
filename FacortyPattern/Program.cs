4b0dfe736110d5efbd4aeb7b5ab912e0289dc66c
Andreas Wassmer <andreas.wassmer@fhnw.ch>
2024-02-06 08:06:22 +0100

    Test class

--- FacortyPattern/Program.cs
+++ FacortyPattern/Program.cs
@@ -1,3 +1,35 @@
 ﻿using System;
 
-namespace FactoryPattern
+namespace FactoryPattern {
+    class Program {
+        static void Main(string[] args)
+        {
+            string cardType = "MoneyBack";
+            CreditCard card = null;
+
+            switch (cardType)
+            {
+                case "MoneyBack":
+                    card = new MoneyBack();
+                    break;
+                case "Titanium":
+                    card = new Titanium();
+                    break;
+                case "Platinum":
+                    card = new Platinum();
+                    break;
+            }
+
+            if (card != null)
+            {
+                Console.WriteLine("Card type    : " + card.GetCardType());
+                Console.WriteLine("Credit limit : " + card.GetCreditLimit());
+                Console.WriteLine("Annual charge: " + card.GetAnnualCharge());
+            }
+            else
+            {
+                Console.WriteLine("Invalid card type.");
+            }
+        }
+    }
+}
